d2193be6204d51f3e11ba6004613054d4fce1cfe
anton2121212 <122678473+anton2121212@users.noreply.github.com>
2024-04-01 11:57:18 +0300

    Add files via upload

---
+++ RPM 2lb/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPM_2lb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаем коллекцию с хэш-таблицами
+            Dictionary<int, string> hashTable = new Dictionary<int, string>();
+
+            // Добавляем элементы в хэш-таблицу
+            hashTable.Add(1, "Адин");
+            hashTable.Add(2, "Два");
+            hashTable.Add(3, "треи");
+            hashTable.Add(4, "патру");
+            hashTable.Add(5, "Пятерочка");
+
+            // Производим поиск по ключу
+            while (true)
+            {
+                Console.WriteLine("Для выхода с кода пропишите 999");
+                Console.Write("Введите значения для проверки: ");
+                int keyToSearch = int.Parse(Console.ReadLine());
+                if (hashTable.ContainsKey(keyToSearch))
+                {
+                    Console.WriteLine($"Значение для ключа {keyToSearch} : {hashTable[keyToSearch]}");
+                    Console.WriteLine(" ");
+                }
+                else if(keyToSearch==999)
+                {
+                    
+                    Console.WriteLine("Вы вышли с кода\nНажмите любую клавишу для закрытия консоли...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Ключ {keyToSearch} не найден в хэш-таблице.");
+                    Console.WriteLine(" ");
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
